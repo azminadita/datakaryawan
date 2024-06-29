a55b81839b98cb6ceb083b95f739dbe1fef081f9
azminadita <azminaditaputri24@gmail.com>
2024-06-29 15:42:41 +0700

    halaman login

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('master');
+// });
+
+// Route::get('/tes-git', function () {
+//     return "Halo, ini cuma coba pakai git";
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
